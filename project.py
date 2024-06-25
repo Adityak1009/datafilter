088ef471a4c4f8f93a1d1e1e9c94bd8df3b13c49
Adityak1009 <30971546+Adityak1009@users.noreply.github.com>
2024-06-25 13:40:34 +0530

    Update project.py

--- project.py
+++ project.py
@@ -28,6 +28,8 @@ if selected_tab == 'Tab 1':
         # Function to display PDF
         def show_pdf(file_path):
             with open(file_path, "rb") as f:
+                file_path
+                st.title(file_path)
                 base64_pdf = base64.b64encode(f.read()).decode('utf-8')
                 pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="800" height="1000" type="application/pdf"></iframe>'
                 st.markdown(pdf_display, unsafe_allow_html=True)
